ae9f3c7fa5238339be99408926a08c3d22c7ce22
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-03-09 10:09:35 -0300

    gulp-uglify e gulp-obsfuscate instalados. tarefa para minificar e codificar os arquivos js adicionada ao gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
 
 function compilarSass() {
   return gulp
@@ -18,6 +20,14 @@ function comprimirImg() {
     .pipe(gulp.dest('./build/img'));
 }
 
+function compilarJs() {
+  return gulp
+    .src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/scripts'));
+}
+
 exports.default = function () {
   gulp.watch(
     './source/styles/*.scss',
@@ -28,5 +38,10 @@ exports.default = function () {
     './source/img/*',
     { ignoreInitial: false },
     gulp.series(comprimirImg),
+  );
+  gulp.watch(
+    './source/scripts/*.js',
+    { ignoreInitial: false },
+    gulp.series(compilarJs),
   );
 };
